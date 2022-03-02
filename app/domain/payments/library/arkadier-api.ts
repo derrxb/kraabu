f704dcd94043e9d32b1edf33a09b2192622c74f0
Derrick Bol <derrickbol33@gmail.com>
2022-03-02 19:36:30 +0800

    feat: arcadier

---
+++ app/domain/payments/library/arkadier-api.ts
@@ -0,0 +1,36 @@
+export type CreatePayment = {
+  /**
+   * Unique identifier for the invoice
+   */
+  invoiceno: string;
+  /**
+   * Currency type. ISO3 code of currency.
+   */
+  currency: string;
+  /**
+   * Total amount to charge.
+   */
+  total: string;
+  /**
+   * Arcadier hashkey
+   */
+  hashkey: string;
+  /**
+   * Your arcadier gateway
+   */
+  gateway: string;
+};
+
+export type CreatePaymentResponse = string;
+
+const getTransactionDetailsURL = ({
+  invoiceno,
+  gateway,
+  hashkey,
+  paykey,
+}: Pick<CreatePayment, "gateway" | "invoiceno" | "hashkey"> & {
+  paykey: string;
+}) =>
+  `https://giggedbz.arcadier.io/user/checkout/order-details?gateway=${gateway}&invoiceNo=${invoiceno}&paykey=${paykey}&hashkey=${hashkey}`;
+
+export { getTransactionDetailsURL };
