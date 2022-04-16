1a50b1a343e2504e2c4ea1285a682b136aa7fea4
Derrick Bol <derrickbol33@gmail.com>
2022-04-16 20:38:10 +0800

    chore: homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,31 @@
+import { MetaFunction } from "remix";
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Krabuu | Online Payments in Belize made simple",
+    description:
+      "Connecting your website to Belize's various E-Wallets have never been easier.",
+  };
+};
+
 export default function Index() {
   return (
-    <div className="h-full w-full flex items-center justify-center">
-      <h1 className="font-bold text-3xl text-gray-800">
-        Quickly integrate and accept online payments in Belize with various
-        supported e-wallets!
-      </h1>
+    <div className="h-full w-full flex flex-col items-center justify-center">
+      <div className="flex flex-col text-left px-4">
+        <h1 className="font-bold text-6xl tracking-widest text-indigo-600">
+          Krabuu Checkout
+        </h1>
+        <h2 className="font-bold text-3xl text-gray-600 tracking-wide">
+          Online Payments in Belize made simple
+        </h2>
+      </div>
+
+      <div>
+        {/* <p>
+          We build Krabuu Checkout so that you don't have to. Easily accept
+          payments with Belize's popular E-wallets!
+        </p> */}
+      </div>
     </div>
   );
 }
