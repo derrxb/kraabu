e0e87400331847fed6ab876c660f960892eeeaa4
Derrick Bol <derrickbol33@gmail.com>
2022-04-23 13:50:36 +0800

    feat: tests

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    threads: false,
+    coverage: {
+      all: true,
+    },
+    environment: "jsdom",
+    setupFiles: ["./test/setup-test-env.ts"],
+  },
+});
