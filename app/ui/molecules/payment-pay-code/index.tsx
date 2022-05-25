c7269197d4215ac848a910330cd1d9f26b517748
Derrick Bol <derrickbol33@gmail.com>
2022-05-25 18:31:52 +0800

    fix: mobile view

--- app/ui/molecules/payment-pay-code/index.tsx
+++ app/ui/molecules/payment-pay-code/index.tsx
@@ -30,7 +30,7 @@ export const PaymentPayCode = ({ qr, paymentMethod, deepLink }: PaymentPayCodePr
         App.
       </h1>
 
-      <span className="text-medium pb-12 text-base text-gray-500">
+      <span className="text-medium pb:8 text-base text-gray-500 md:pb-12">
         Scan the QR code to complete payment with your E-Kyash app.
       </span>
 
