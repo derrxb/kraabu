b6a545b7c2e4b32183fd20d706e84965774781d7
Derrick Bol <derrickbol33@gmail.com>
2022-06-08 16:29:21 +0800

    deps: remove supabase

--- app/config/index.server.ts
+++ app/config/index.server.ts
@@ -1,9 +1,3 @@
-import { createClient } from '@supabase/supabase-js';
-import 'dotenv/config';
+import noop from 'lodash/noop';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
-
-export { supabase };
+export { noop };
