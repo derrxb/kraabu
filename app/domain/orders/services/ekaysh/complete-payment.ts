b44af8b6efbe66b5c34a639ab39945fbd41fbe87
Derrick Bol <derrickbol33@gmail.com>
2022-06-08 10:01:15 +0800

    docs: log failed payment request

--- app/domain/orders/services/ekaysh/complete-payment.ts
+++ app/domain/orders/services/ekaysh/complete-payment.ts
@@ -25,6 +25,7 @@ export default class CompletePayment {
 
   async verifyPaymentParams() {
     const body = await this.request.json();
+    console.log('COMPLETED PAYMENT => ', body);
     const validatedParams = await completePendingEkyashPaymentSchema.validateAsync({
       ...body,
     });
