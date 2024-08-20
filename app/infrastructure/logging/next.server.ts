3623c3cc649848e59f1cac2d7beb8859fd9805a5
Derrick Bol <derrickbol33@gmail.com>
2024-08-21 00:34:22 +0800

    fix: logger

--- app/infrastructure/logging/next.server.ts
+++ app/infrastructure/logging/next.server.ts
@@ -3,12 +3,19 @@ import pinoPretty from 'pino-pretty';
 import logtail from '@logtail/pino';
 
 const token = process.env.BETTER_STACK_LOGS;
-let logTail;
-if (process.env.NODE_ENV === 'production') {
-  logTail = await logtail({ sourceToken: token!, options: {} });
-}
 
-export const logger = pino(
-  { base: null },
-  process.env.NODE_ENV === 'production' ? logTail : pinoPretty({ sync: true }),
-);
+const getLogger = async () => {
+  let logTail;
+  if (process.env.NODE_ENV === 'production') {
+    const setLogTail = async () => {
+      logTail = await logtail({ sourceToken: token!, options: {} });
+    };
+
+    setLogTail();
+  }
+
+  const logger = pino({ base: null }, process.env.NODE_ENV === 'production' ? logTail : pinoPretty({ sync: true }));
+  return logger;
+};
+
+export const logger = await getLogger();
