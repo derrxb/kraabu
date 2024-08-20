30638e192cd32acaa5cc1e11de619836fd312736
Derrick Bol <derrickbol33@gmail.com>
2024-08-20 21:07:57 +0800

    fix: disable pino

--- app/infrastructure/logging/next.server.ts
+++ app/infrastructure/logging/next.server.ts
@@ -1,12 +1,11 @@
-import pinoConstructor from 'pino';
+// import pinoConstructor from 'pino';
 
-const token = process.env.BETTER_STACK_LOGS;
-const transport = pinoConstructor.transport({
-  // target: process.env.NODE_ENV !== 'production' ? 'pino-pretty' : '@logtail/pino',
-  target: 'pino-pretty',
-  options: {
-    sourceToken: token,
-  },
-});
+// const token = process.env.BETTER_STACK_LOGS;
+// const transport = pinoConstructor.transport({
+//   target: process.env.NODE_ENV !== 'production' ? 'pino-pretty' : '@logtail/pino',
+//   options: {
+//     sourceToken: token,
+//   },
+// });
 
-export const logger = pinoConstructor(transport);
+export const logger = console;
