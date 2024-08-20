b54ce66adcfecd459b307c95b6405ec5d20394d4
Derrick Bol <derrickbol33@gmail.com>
2024-08-20 21:05:10 +0800

    fix: imports

--- app/infrastructure/logging/next.server.ts
+++ app/infrastructure/logging/next.server.ts
@@ -2,7 +2,8 @@ import pinoConstructor from 'pino';
 
 const token = process.env.BETTER_STACK_LOGS;
 const transport = pinoConstructor.transport({
-  target: process.env.NODE_ENV !== 'production' ? 'pino-pretty' : '@logtail/pino',
+  // target: process.env.NODE_ENV !== 'production' ? 'pino-pretty' : '@logtail/pino',
+  target: 'pino-pretty',
   options: {
     sourceToken: token,
   },
